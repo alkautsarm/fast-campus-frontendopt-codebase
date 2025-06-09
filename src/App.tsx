633d9f855a87c6f851003d02d319b210824c6199
Muhammad Al-Kautsar Maktub <alkautsar.maktub@shopee.com>
2025-06-10 01:04:03 +0700

    feat: add explore page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,103 @@
+import {
+  query,
+  startAfter,
+  limitToFirst,
+  orderByKey,
+  onValue,
+  ref,
+} from "firebase/database";
+import { CSSProperties, memo, useCallback, useEffect, useState } from "react";
+import { db } from "./utils";
+import HotelCard from "./components/HotelCard";
+import { IHotelData } from "./types";
+import InfiniteLoader from "react-window-infinite-loader";
+import { FixedSizeList } from "react-window";
+
+const limit = 5;
+
+const HotelRow = memo(
+  ({
+    data,
+    index,
+    style,
+  }: {
+    data: IHotelData[];
+    index: number;
+    style: CSSProperties;
+  }) => (
+    <div style={style}>
+      <HotelCard data={data[index]} />
+    </div>
+  ),
+);
+
 function App() {
+  const [hotels, setHotels] = useState<IHotelData[]>([]);
+  const [lastItemKey, setLastItemKey] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const loadHotels = useCallback(
+    (after?: string) => {
+      if (loading) return;
+
+      setLoading(true);
+
+      const queryConstraints = [limitToFirst(limit), orderByKey()];
+
+      if (after) {
+        queryConstraints.push(startAfter(after));
+      }
+
+      const hotelsQuery = query(ref(db, "hotels"), ...queryConstraints);
+
+      onValue(hotelsQuery, (snapshot) => {
+        if (snapshot.exists()) {
+          const hotelsKey = Object.keys(snapshot.val());
+          setLastItemKey(hotelsKey[hotelsKey.length - 1]);
+
+          const hotelsData = Object.values(snapshot.val()) as IHotelData[];
+          setHotels((prev) =>
+            after ? [...prev, ...hotelsData] : [...hotelsData],
+          );
+        }
+
+        setLoading(false);
+      });
+    },
+    [loading],
+  );
+
+  useEffect(() => {
+    loadHotels();
+  }, [loadHotels]);
+
   return (
-    <main>
-      Hello World
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Explore</h1>
+
+      <section className="flex flex-col gap-6">
+        <InfiniteLoader
+          isItemLoaded={(index) => !loading && !!hotels[index]}
+          itemCount={1000}
+          loadMoreItems={() => loadHotels(lastItemKey || undefined)}
+        >
+          {({ onItemsRendered, ref }) => (
+            <FixedSizeList
+              itemSize={450}
+              itemCount={hotels.length}
+              itemData={hotels}
+              onItemsRendered={onItemsRendered}
+              ref={ref}
+              width="100%"
+              height={window.innerHeight - 80}
+            >
+              {HotelRow}
+            </FixedSizeList>
+          )}
+        </InfiniteLoader>
+      </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/HotelCard.tsx
@@ -0,0 +1,76 @@
+import { Heart, Star } from "lucide-react";
+import { IHotelData } from "../types";
+import { memo, useState } from "react";
+
+interface IHotelCardProps {
+  data: IHotelData;
+}
+
+const formatDate = (s: string, e: string) => {
+  const start = new Date(s);
+  const end = new Date(e);
+
+  const startMonth = start.toLocaleString("en-US", { month: "short" });
+  const endMonth = end.toLocaleString("en-US", { month: "short" });
+
+  const startDate = start.getDate();
+  const endDate = end.getDate();
+
+  const startYear = start.getFullYear();
+  const endYear = end.getFullYear();
+
+  if (startMonth === endMonth && startYear === endYear) {
+    return `${startMonth} ${startDate} - ${endDate}`;
+  }
+
+  if (startYear === endYear) {
+    return `${startMonth} ${startDate} - ${endMonth} ${endDate}`;
+  }
+
+  return `${startMonth} ${startDate}, ${startYear} - ${endMonth} ${endDate}, ${endYear}`;
+};
+
+function HotelCard({ data }: IHotelCardProps) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
+  return (
+    <div>
+      <div className="mb-4 relative">
+        <img
+          className="h-[310px] w-full rounded-xl object-cover"
+          src={data.imageUrl}
+        />
+        <button className="absolute top-3 right-3" onClick={handleLike}>
+          <Heart className="text-white" fill={isLiked ? "white" : "gray"} />
+        </button>
+      </div>
+
+      <div>
+        <div className="mb-1 flex items-center justify-between">
+          <div className="font-medium">{data.location}</div>
+          <div className="flex items-center gap-1">
+            <Star className="w-3.5" />
+            <span>
+              {data.rating} ({data.reviews})
+            </span>
+          </div>
+        </div>
+        <div className="mb-1 text-sm text-gray-400">
+          {data.distance} kilometers
+        </div>
+        <div className="mb-2 text-sm text-gray-400">
+          {formatDate(data.availableDates.start, data.availableDates.end)}
+        </div>
+        <div>
+          <span className="font-semibold">${data.pricePerNight}</span> night
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default memo(HotelCard);
