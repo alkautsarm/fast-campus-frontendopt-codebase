a31afcb9d7b3ca9522c3033dbc79f7a59495b40c
Muhammad Al-Kautsar Maktub <alkautsarm@gmail.com>
2025-07-07 05:13:53 +0700

    feat: remove unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,6 @@ const inter = Inter({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   style: ["normal", "italic"],
-  display: "swap",
 });
 
 export default function RootLayout({
